015d23b4283ec199f4aed3938995f3ad3444625a
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2022-10-23 11:59:13 -0700

    create RESTful routes and HTML Forms to add new products

--- index.js
+++ index.js
@@ -17,11 +17,23 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 
 // await mongoose using async functions
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products })
+})
+
+//create new product route
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+//route after submit the new product form
+app.post('products', (req, res) => {
+    console.log(req.body)
+    res.send('making your product!')
 })
 
 app.get('/products/:id', async (req, res) => {
