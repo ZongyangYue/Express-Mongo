412ddded3844d18c39f6fafcf775af2b7e598ee8
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2022-10-23 11:38:43 -0700

    add individual product show pages and all products pages and routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 const Product = require('./models/product');
+const { rmSync } = require('fs');
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
     .then(() => {
@@ -17,6 +18,17 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// await mongoose using async functions
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id)
+    res.render('products/show', { product })
+})
 app.listen(3030, () => {
     console.log("Listening on port 3030!")
 })
