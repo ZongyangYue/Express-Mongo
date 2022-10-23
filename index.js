eb95e2ad9defe4e041c9cdb94cc768f4a6597489
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2022-10-23 12:34:08 -0700

    create RESTful routes and HTML/EJS Form to update product

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
 
 const Product = require('./models/product');
 const { rmSync } = require('fs');
@@ -17,7 +19,9 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
 // await mongoose using async functions
 app.get('/products', async (req, res) => {
@@ -42,6 +46,19 @@ app.get('/products/:id', async (req, res) => {
     const product = await Product.findById(id)
     res.render('products/show', { product })
 })
+
+//route to the edit product form
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    console.log(req.body);
+    res.send('PUT!')
+})
+
 app.listen(3030, () => {
     console.log("Listening on port 3030!")
 })
