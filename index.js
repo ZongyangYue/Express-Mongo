ad241f69f172502ece5b4c9c0d5b081b6231caa4
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2022-10-23 15:41:42 -0700

    add RESTful delete route to delete a product

--- index.js
+++ index.js
@@ -62,8 +62,11 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`)
 })
 
-app.delete('/product/:id', async (req, res) => {
-    res.send("made it")
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+
 })
 
 app.listen(3030, () => {
